c30a3d25b1e367fd073373c52fc8e29019a29d94
navibryle <naviivan321@gmail.com>
2024-05-21 13:23:37 -0600

    remove bloat

--- src/App.tsx
+++ src/App.tsx
@@ -33,25 +33,6 @@ function App() {
               I am working on a team that is maintaining and adding new features to <Link target="_blank" rel="noopener" href="https://plasma.haemonetics.com/software-solutions/nexlynk-dms">Nexlynk DMS</Link>.<br/>
               I write Java on a day to day basis, with frameworks such as spring and hibernate.<br/>
             </Typography>
-            <Typography variant='h4'>How do I function in my team?</Typography>
-              <Typography paragraph={true}>
-                  My team is focused on implementing user requested features and fixing existing bugs.<br/>
-                  I work on both features and bugs.<br/>
-                  I participate in design meetings. <br/>
-                  I create design documents.<br/>
-                  I work on our code infrastructure such us our build tools and our CICD pipelines.<br/>
-                  I work on verifying security concerns raised by <Link target="_blank" rel="noopener" href="https://www.veracode.com/">Veracode</Link>.<br/>
-                  I work on internal tooling.<br/>
-              </Typography>
-            <Typography variant='h4'>How would I describe myself?</Typography>
-            <div className="flex justify-center">
-              <Typography paragraph={true} width={500}>
-                I am someone who is accustomed to the challenges of creating "good" software. I know 
-                how to work in a team. I have some experience on estimating my own ability. 
-                I also have some experience on when to deny scope creep and when to accept it. I am also not just a developer whose focused on producing code,
-                I am a developer whose focused on producing "good" software.
-              </Typography>
-            </div>
               <div className="flex justify-center m-5">
                 <ButtonGroup orientation="vertical">
                   <Button href="#projects" className="hidden">Projects</Button>
